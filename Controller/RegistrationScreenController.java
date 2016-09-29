88c1f9afb5de70f24eece888d879ba083ae472f9
Swati <swatimardia@gmail.com>
2016-09-28 22:12:47 -0400

    Controller made

--- Controller/RegistrationScreenController.java
+++ Controller/RegistrationScreenController.java
@@ -19,9 +19,30 @@ public class RegistrationScreenController {
 	 * reference to main application
 	 */
 	private MainFXApp mainApp;
+
+    @FXML
     private TextField textField_UserName;
+
+    @FXML
     private TextField textField_Pass;
+
+    @FXML
 	private ComboBox<String> comboBox_Type;
+
+
+
+    @FXML
+    private void initialize() {
+        ArrayList<String> arrayList = new ArrayList();
+        arrayList.add("User");
+        arrayList.add("Worker");
+        arrayList.add("Manager");
+        arrayList.add("Admin");
+        ObservableList<String> list = FXCollections.observableArrayList(arrayList);
+        comboBox_Type.setItems(list);
+    }
+
+
 	/**
 	 * Setup the main application link so we can call methods there
 	 * @param main reference to main application
@@ -42,7 +63,20 @@ public class RegistrationScreenController {
 	private void handleRegisterPressed() {
 	    String userName = textField_UserName.getText();
         String pass = textField_Pass.getText();
-        // check account type and then instantiate
+        String type = comboBox_Type.getValue();
+        AuthorizedUser user;
+
+        if (type == "User") {
+            user = new User(userName, pass);
+        } else if (type == "Worker") {
+            user = new Worker(userName, pass);
+        } else if (type == "Manager") {
+            user = new Manager(userName, pass);
+        } else if (type == "Admin") {
+            user = new Admin(userName, pass);
+        }
+
+        Model.addAuthorizedUser(user);
 
     }
 }
