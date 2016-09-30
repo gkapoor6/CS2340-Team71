040342c2083a5e193641a2fb2ff4b96b9e336b5e
kehairong <kehairong@lawn-128-61-68-195.lawn.gatech.edu>
2016-09-30 16:27:09 -0400

    profile controller updated

--- Controller/ProfileScreenController.java
+++ Controller/ProfileScreenController.java
@@ -1,35 +1,103 @@
 package Controller;
 
-import FXApp.MainFXApp;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import FXApp.MainFXApp;
+import Model.Profile;
 
 public class ProfileScreenController {
 	private MainFXApp mainApp;
-	
+
 	@FXML
-	private TextField
-	
+	private TextField nameField;
+
 	@FXML
-	private TextField
-	
+	private ComboBox<String> titleComboBox;
+
 	@FXML
-	private TextField
-	
+	private TextField emailField;
+
 	@FXML
-	private TextField
-	
+	private TextField addressField;
+
+	/** the window for this dialog */
+	private Stage dialogStage;
+
+	/** the student whose data is being manipulated */
+	private Profile profile;
+
+	/** flag to signal whether dialog was closed normally */
+	private boolean uodateClicked = false;
+
 	public void setMainApp(MainFXApp main) {
 		mainApp = main;
 	}
-	
+
+	/**
+	 * Returns true if the update button is clicked.
+	 *
+	 * @return true if the update button clicked.
+	 */
+	public boolean isOkClicked() {
+		return uodateClicked;
+	}
+
+	@FXML
+	private void handleUpdatePressed() {
+		// validate the profile info
+		if (isInputValid()) {
+			profile.setEmail(emailField.getText());
+			profile.setName(nameField.getText());
+			profile.setHome(addressField.getText());
+
+			// Signal success and close the profile dialog window.
+			uodateClicked = true;
+			mainApp.showApplication();
+		}
+	}
+
 	@FXML
 	private void handleCancelPressed() {
 		mainApp.showApplication();
 	}
-	
-	@FXML
-	private void handleUpdatePressed() {
-		
-		mainApp.showApplication();
+
+	/**
+	 * Validate the text fields
+	 * 
+	 * @return true if the input is valid
+	 */
+
+	private boolean isInputValid() {
+		String errorMessage = "";
+		if (nameField.getText() == null || nameField.getText().length() == 0) {
+			errorMessage += "Please enter a valid name.\n";
+		}
+		if (emailField.getText() == null || emailField.getText().length() == 0) {
+			errorMessage += "Please enter a valid name.\n";
+		}
+		if (addressField.getText() == null
+				|| addressField.getText().length() == 0) {
+			errorMessage += "Please enter a valid address.\n";
+		}
+
+		// no error message means success / good input
+		if (errorMessage.length() == 0) {
+			return true;
+		} else {
+			// Show the error message if bad data
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.initOwner(dialogStage);
+			alert.setTitle("Invalid Fields");
+			alert.setHeaderText("Please correct invalid fields");
+			alert.setContentText(errorMessage);
+
+			alert.showAndWait();
+
+			return false;
+		}
+
 	}
 }
--- Model/Profile.java
+++ Model/Profile.java
@@ -7,26 +7,42 @@ import javafx.beans.property.StringProperty;
  * Created by Geetika Kapoor on 9/28/2016.
  */
 public class Profile {
-    private StringProperty _name = new SimpleStringProperty();
-    private StringProperty _email = new SimpleStringProperty();
-    // title or type
-    private StringProperty _title = new SimpleStringProperty();
-    private StringProperty _home = new SimpleStringProperty();
+	private StringProperty _name = new SimpleStringProperty();
+	private StringProperty _email = new SimpleStringProperty();
+	// title or type
+	private StringProperty _title = new SimpleStringProperty();
+	private StringProperty _home = new SimpleStringProperty();
 
-    public Profile(String name, String email, String title, String home) {
-        _name.set(name);
-        _email.set(email);
-        _title.set(title);
-        _home.set(home);
-    }
+	public Profile(String name, String email, String title, String home) {
+		_name.set(name);
+		_email.set(email);
+		_title.set(title);
+		_home.set(home);
+	}
 
-    public Profile() {
-    	this("", "", "", "");
-    }
+	public Profile() {
+		this("", "", "", "");
+	}
 
-    public void setName(String name) { _name.set(name); }
-    public void setEmail(String email) { _email.set(email); }
-    public void setTitle(String title) { _title.set(title); }
-    public void setHome(String home) { _home.set(home); }
-    
+	public void setName(String name) {
+		_name.set(name);
+	}
+
+	public void setEmail(String email) {
+		_email.set(email);
+	}
+
+	public void setTitle(String title) {
+		_title.set(title);
+	}
+
+	public void setHome(String home) {
+		_home.set(home);
+	}
+
+	public String toString() {
+		return "Profile [_name=" + _name + ", _email=" + _email + ", _title="
+				+ _title + ", _home=" + _home + "]";
+	}
+
 }
